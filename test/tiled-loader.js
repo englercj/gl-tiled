22f3839a013f6e6401958cd97458efef2e44e535
Chad Engler <englercj@live.com>
2017-10-25 08:43:35 -0700

    add test maps and a test page

---
+++ test/tiled-loader.js
@@ -0,0 +1,55 @@
+(function (exports) {
+    exports.tiledMiddlewareFactory = function ()
+    {
+        return function tiledMiddleware(resource, next)
+        {
+            if (!resource.data
+                || resource.type !== Loader.Resource.TYPE.JSON
+                || !resource.data.layers
+                || !resource.data.tilesets
+            )
+            {
+                next();
+                return;
+            }
+
+            const loadOptions = {
+                crossOrigin: resource.crossOrigin,
+                loadType: Loader.Resource.LOAD_TYPE.IMAGE,
+                metadata: resource.metadata.imageMetadata,
+                parentResource: resource,
+            };
+
+            let urlDir = resource.url.replace(this.baseUrl, '');
+
+            urlDir = urlDir.substr(0, urlDir.lastIndexOf('/')) + '/';
+
+            for (let i = 0; i < resource.data.tilesets.length; ++i)
+            {
+                const tileset = resource.data.tilesets[i];
+
+                if (tileset.image)
+                {
+                    if (!this.resources[tileset.image])
+                    {
+                        this.add(tileset.image, urlDir + tileset.image, loadOptions);
+                    }
+                }
+                else if (resource.data.tiles)
+                {
+                    for (const key in resource.data.tiles)
+                    {
+                        const tile = resource.data.tiles[key];
+
+                        if (tile.image && !this.resources[tile.image])
+                        {
+                            this.add(tile.image, urlDir + tile.image, loadOptions);
+                        }
+                    }
+                }
+            }
+
+            next();
+        }
+    }
+})(this || window);
